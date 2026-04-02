ebe53f8c06ea36423be43e2d84373a2d213c0beb
MinMinCai <atat465055@gmail.com>
2026-04-02 11:00:14 +0800

    fix: enhance request handling in API by adding headers processing and writable stream support

--- api/index.js
+++ api/index.js
@@ -1,4 +1,40 @@
-import { createRequestHandler } from "@vercel/remix";
+import { createRequestHandler, writeReadableStreamToWritable } from "@remix-run/node";
 import * as build from "../build/server/index.js";
 
-export default createRequestHandler({ build });
+const handler = createRequestHandler(build);
+
+export default async function (req, res) {
+  const host = req.headers["x-forwarded-host"] || req.headers["host"] || "localhost";
+  const protocol = req.headers["x-forwarded-proto"] || "https";
+  const url = new URL(`${protocol}://${host}${req.url}`);
+
+  const headers = new Headers();
+  for (const [key, value] of Object.entries(req.headers)) {
+    if (Array.isArray(value)) {
+      for (const v of value) headers.append(key, v);
+    } else if (value != null) {
+      headers.set(key, value);
+    }
+  }
+
+  const init = { method: req.method, headers };
+  if (req.method !== "GET" && req.method !== "HEAD") {
+    init.body = req;
+    init.duplex = "half";
+  }
+
+  const request = new Request(url.href, init);
+  const response = await handler(request);
+
+  res.statusCode = response.status;
+  res.statusMessage = response.statusText;
+  for (const [key, value] of response.headers) {
+    res.appendHeader(key, value);
+  }
+
+  if (response.body) {
+    await writeReadableStreamToWritable(response.body, res);
+  } else {
+    res.end();
+  }
+}
